a36f01d9b7e8841df310c10b9755bd289b2445a4
Ani <turboji007@gmail.com>
2026-02-07 15:23:05 +0530

    feat : added ability to create project index file based on use drsired choice of lenguage ts/js

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,101 @@
 #!/usr/bin/env node
 
-import fs from 'fs'
+import fs from "fs";
+import inquirer from "inquirer";
 
+// const projectName = process.argv[2];
 
+// if (!projectName) {
+//   console.log("❌ Please provide a project name");
+//   console.log("Usage: create-node-stack <project-name>");
+//   process.exit(1);
+// }
 
-const projectName = process.argv[2];
+// const projectPath = `./${projectName}`;
+// if (fs.existsSync(projectPath)) {
+//   console.log("❌ Folder already exists");
+//   process.exit(1);
+// }
 
-if (!projectName) {
-  console.log("❌ Please provide a project name");
-  console.log("Usage: create-node-stack <project-name>");
-  process.exit(1);
-}
+// fs.mkdirSync(projectPath)
+// console.log("📁 Project folder created:", projectName);
 
-const projectPath = `./${projectName}`;
-if (fs.existsSync(projectPath)) { 
-  console.log("❌ Folder already exists");
-  process.exit(1);
-}
+// // package.json file creation
+// const packageJson = {
+//   name: projectName,
+//   version: "0.0.1",
+//   private: true,
+//   scripts: {
+//     dev: "node src/index.js",
+//     start: "node src/index.js"
+//   }
+// };
 
-fs.mkdirSync(projectPath)
-console.log("📁 Project folder created:", projectName);
+// fs.writeFileSync(
+//   `${projectPath}/package.json`,
+//   JSON.stringify(packageJson,null,2)
+// )
+// console.log("📦 package.json created");
 
-// package.json file creation
-const packageJson = {
-  name: projectName,
-  version: "0.0.1",
-  private: true,
-  scripts: {
-    dev: "node src/index.js",
-    start: "node src/index.js"
+async function main() {
+  const projectName = process.argv[2];
+
+  if (!projectName) {
+    console.log("❌ Please provide a project name");
+    process.exit(1);
   }
-};
+
+  const projectPath = `./${projectName}`;
+
+  if (fs.existsSync(projectPath)) {
+    console.log("❌ Folder already exists");
+    process.exit(1);
+  }
+
+  // create project filder
+  fs.mkdirSync(projectPath);
+  console.log("📁 Project folder created:", projectName);
 
 
-fs.writeFileSync(
-  `${projectPath}/package.json`,
-  JSON.stringify(packageJson,null,2)
+  // ask for programing lenguage
+  const { language } = await inquirer.prompt([
+    {
+      type: "list",
+      name: "language",
+      message: "Which language do you want to use?",
+      choices: ["TypeScript", "JavaScript"]
+    },
+  ]);
 
-)
-console.log("📦 package.json created");
+
+  const isTS = language == "TypeScript";
+  const ext = isTS ? "ts" : "js";
+
+
+  // content of the project's package.json file
+  const packageJson = {
+    name: projectName,
+    version: "0.0.1",
+    private: true,
+    scripts: isTS
+      ? { dev: "ts-node src/index.ts" }
+      : { dev: "node src/index.js" },
+  };
+
+  // create the packege.json file and insert the content into it
+  fs.writeFileSync(
+    `${projectPath}/package.json`,
+    JSON.stringify(packageJson, null, 2),
+  );
+
+  // create src folder
+  // fs.mkdirSync(`${projectPath}/src`)
+
+  //create and write content inside index file (under src folder) for the project
+  const indexFileContent = `console.log('hello form ${projectName}')`;
+  fs.writeFileSync(`./${projectName}/src/index.${ext}`, indexFileContent);
+
+  console.log("You chose:", language);
+}
+
+main();
